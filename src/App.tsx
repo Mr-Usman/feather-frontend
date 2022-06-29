d95d85a90346555ddc6bdd18bea5bc70032f20c4
Mr-Usman <usmanrehmat15@outlook.com>
2022-06-30 02:55:46 +0500

    setup all static files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Table from "./components/Table";
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+      <div className="w-full p-8">
+        <Header />
+        <Table />
+      </div>
+    </>
+  );
 }
 
 export default App;
